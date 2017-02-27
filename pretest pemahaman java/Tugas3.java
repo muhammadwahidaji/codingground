b9f12d4a21978d6ebc55dc647a68fec5e84db4fd
muhammadwahidaji <mwafr123@gmail.com>
2017-02-27 10:10:40 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class tugas3{
+
+     public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("masukkan angka : ");
+        byte number = scanner.nextByte();
+        System.out.println("yang anda masukkan adalah angka" + number);
+     }
+}
