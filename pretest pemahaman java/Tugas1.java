5f7c78973efc2ab1baa293756701eed59737f9e4
muhammadwahidaji <mwafr123@gmail.com>
2017-02-27 10:08:12 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1{
+
+     public static void main(String []args){
+         String nama = "Aji";
+         String kota = "Jepara";
+        System.out.println("Nama = "+nama+" Kota = "+kota);
+     }
+}
