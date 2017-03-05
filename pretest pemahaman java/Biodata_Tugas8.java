915a9f91b3de355de6d7ca18d9acd7c3dfc94fc6
muhammadwahidaji <mwafr123@gmail.com>
2017-03-05 23:30:23 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Biodata_Tugas8.java
@@ -0,0 +1,11 @@
+public class Biodata_Tugas8{
+	String nama = "Wahid Aji";
+	String alamat = "Mlonggo";
+	int umur = 21;
+
+	void CetakBiodata(){
+		System.out.println("Nama    : " + nama );
+		System.out.println("Alamat  : " + alamat );
+		System.out.println("Umur    : " + umur );
+	}
+}
