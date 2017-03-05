7244b1aaa340dab868f4c2aea487171ef93ec57e
muhammadwahidaji <mwafr123@gmail.com>
2017-03-05 23:39:29 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas2.java
@@ -0,0 +1,11 @@
+public class Tugas2{
+    
+    public static void main(String[]args){
+        String[] Array={ "Aji","Aja","AJE","AJU" };
+        
+         for (int i = 0; i < Array.length; i++) {
+         System.out.println(Array[i] + " ");
+        }
+        
+    }
+}
