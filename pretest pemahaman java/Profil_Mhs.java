692ebc87b8c5c4ef4cad4e7bc623b543696c452b
muhammadwahidaji <mwafr123@gmail.com>
2017-03-05 23:55:26 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Profil_Mhs.java
@@ -0,0 +1,8 @@
+public class Profil_Mhs extends Biodata_Tugas11{
+	String nim = "141240000305";
+	String jurusan = "Teknik Informatika";
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur + "\nNIM : " + nim + "\nJurusan : " + jurusan);
+	}
+}
